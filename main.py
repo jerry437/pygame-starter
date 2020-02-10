fcaed8baa9f5d91f55f8d931f96d2e5d65a2ad27
jerry437 <50630964+jerry437@users.noreply.github.com>
2020-02-10 10:58:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ pygame.init()
 
 # Create the window
 win = pygame.display.set_mode((800, 600))
-
+x = 0
+y = 0
 run = True
 while run:
   for event in pygame.event.get():
@@ -12,9 +13,10 @@ while run:
 
 # Game code starts here ---------------------
   win.fill((0, 0, 0))
-
+  x += 1
+  y +=0.1
   # Draw a rectangle
-  pygame.draw.rect(win, (0, 204, 102), (50, 50, 100, 200))
+  pygame.draw.rect(win, (0, 204, 102), (x, y, 200, 200))
   
   #Update the display
   pygame.display.update()
