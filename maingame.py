f644d246b509bfcbb5e2026229ad7c0cb68243fe
jerry437 <50630964+jerry437@users.noreply.github.com>
2020-02-14 11:16:08 -0500

    Update maingame.py

--- maingame.py
+++ maingame.py
@@ -8,21 +8,34 @@ img = pygame.transform.scale(img, (50, 50))
 background = pygame.transform.scale(background, (800, 600))
 x = 200
 y = 200
-
+hp = 100
 run = True
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-  #keys = pygame.key.get_pressed()
-  #if keys[pygame.K_LEFT]:
-   # x -= 1
-   #DRAWS THE IMAGE,TEXT
-    win.fill((0, 0, 0))
-    win.blit(background,(0,0))
-    win.blit(img, (x, y))
-  
+    if img < 400:
+        hp -= 1
+
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        x -= 1
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_RIGHT]:
+        x += 1
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        x += 1
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_DOWN]:
+        x -= 1
+    # DRAWS THE IMAGE,TEXT
+        win.fill((0, 0, 0))
+        win.blit(background,(0,0))
+        win.blit(img, (x, y))
+        win.blit(hp(0,3))
 
     pygame.display.update()
 
